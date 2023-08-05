80d68070ec357e4a9bed72fd51298129030388e1
Vanani Jaydip <132327495+jaydipvanani@users.noreply.github.com>
2023-08-05 18:36:58 +0530

    Update find-neutral-.c

--- find-neutral-.c
+++ find-neutral-.c
@@ -7,7 +7,7 @@ void main ()
 
     float a;
     
-    printf("1. YOU WILL GIVEN TWO ATTEMPT FROM WHICH YOU ATTEMPT FROM WHICH YOU ENTER THE NEUTRAL NUMBER \n");
+    printf("1. YOU WILL GIVEN TWO ATTEMPT FROM WHICH YOU OPTION FROM WHICH YOU ENTER THE NEUTRAL NUMBER \n");
     
     printf("Enter Value Of A = ");
     scanf("%f",&a);
